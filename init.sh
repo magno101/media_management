0b6fb31a688d494f86b7f72c329664f4d5528a95
magno101 <magno101@gmail.com>
2022-05-26 00:58:24 -0500

    added a docker compose stop command before the copies

--- init.sh
+++ init.sh
@@ -2,9 +2,11 @@
 
 docker-compose up -d
 
+docker-compose stop
+
 docker cp ./custom/pia/. pia:/pia
 docker cp ./custom/sonarr/config/. sonarr:/config
 docker cp ./custom/radarr/config/. radarr:/config
 docker cp ./custom/lidarr/config/. lidarr:/config
 
-docker-compose restart
+docker-compose start
